88f4550ccda00af6f4ea3aa5e03cab8846d42158
salim khan <salimssm121@gmail.com>
2020-05-04 12:31:55 +0530

    Admin dropdown Added

---
+++ Membership_/Scripts/AdminMenu.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('[data-admin-menu]').hover(function () {
+        $('[data-admin-menu]').toggleClass('open');
+    });
+    $('[data-admin-menu] ul a').mouseover(function () {
+        $(this).css('background-color','#337ab7');
+    });
+    $('[data-admin-menu] ul a').mouseleave(function () {
+        $(this).css('background-color', 'white');
+    });
+});
