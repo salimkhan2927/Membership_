7e428d3fdb3535426616741a704eff2aea390760
salim khan <salimssm121@gmail.com>
2020-05-04 15:11:10 +0530

    Database added

---
+++ Membership_/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Membership_.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Membership_.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Membership_.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
