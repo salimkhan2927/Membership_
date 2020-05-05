2f110cc84333ac041a8ba94fed8780bf3b95d90b
salim khan <salimssm121@gmail.com>
2020-05-05 11:37:20 +0530

    ProductType ad ProductLinkText Tables Added

---
+++ Membership_/Entities/ProductLinktext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Membership_.Entities
+{
+    [Table("ProductLinkText")]
+    public class ProductLinkText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Title { get; set; }
+    }
+}
