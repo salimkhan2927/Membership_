d5e7d8cfd1b403e47000cb0fcad7425b85683c77
salim khan <salimssm121@gmail.com>
2020-05-05 10:48:20 +0530

    Item Table Added

--- Membership_/Models/IdentityModels.cs
+++ Membership_/Models/IdentityModels.cs
@@ -24,6 +24,7 @@ namespace Membership_.Models
         public DbSet<Section> Sections { get; set; }
         public DbSet<Part> Parts { get; set; }
         public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Item> Items { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
