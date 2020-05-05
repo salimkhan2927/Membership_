3739f722ed379212134cddd412821c8ceee1b794
salim khan <salimssm121@gmail.com>
2020-05-05 12:09:42 +0530

    SubscriptionProduct Table Added

--- Membership_/Models/IdentityModels.cs
+++ Membership_/Models/IdentityModels.cs
@@ -30,7 +30,8 @@ namespace Membership_.Models
         public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<ProductItem> ProductItems { get; set; }
-        
+        public DbSet<SubscriptionProduct> SubscriptionProducts { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
