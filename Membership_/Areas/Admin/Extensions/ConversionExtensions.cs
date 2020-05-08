6c37eb58dc6a57c7bb8b1f6311437226a95f207f
salim khan <salimssm121@gmail.com>
2020-05-08 13:02:54 +0530

    Conversion Extension method added to admin areas

---
+++ Membership_/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Membership_.Areas.Admin.Models;
+using Membership_.Entities;
+using Membership_.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Membership_.Areas.Admin.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(this IEnumerable<Product> products,ApplicationDbContext db )
+        {
+            var types = await db.ProductTypes.ToListAsync();
+            var texts = await db.ProductLinkTexts.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+                   };
+        }
+    }
+}
